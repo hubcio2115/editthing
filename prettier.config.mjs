26cdf85fa3d048c2a7124bb6863ea3e5586726d5
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-10-03 19:03:54 +0200

    adde prettier plugins

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "@trivago/prettier-plugin-sort-imports",
+  ],
 };
 
 export default config;
