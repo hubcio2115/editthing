6800c8c251c9948ed6f9a9e38587d2103d49cefa
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-10-06 12:47:15 +0200

    migrated whole project to app dir

--- .drizzle/0000_hesitant_magik.sql
+++ .drizzle/0000_hesitant_magik.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `group-project_account` (
+CREATE TABLE `t3-app_account` (
 	`userId` varchar(255) NOT NULL,
 	`type` varchar(255) NOT NULL,
 	`provider` varchar(255) NOT NULL,
@@ -10,40 +10,42 @@ CREATE TABLE `group-project_account` (
 	`scope` varchar(255),
 	`id_token` text,
 	`session_state` varchar(255),
-	CONSTRAINT `group-project_account_provider_providerAccountId` PRIMARY KEY(`provider`,`providerAccountId`)
+	CONSTRAINT `t3-app_account_provider_providerAccountId` PRIMARY KEY(`provider`,`providerAccountId`)
 );
 --> statement-breakpoint
-CREATE TABLE `group-project_example` (
+CREATE TABLE `t3-app_post` (
 	`id` bigint AUTO_INCREMENT NOT NULL,
 	`name` varchar(256),
+	`createdById` varchar(255) NOT NULL,
 	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updatedAt` timestamp ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT `group-project_example_id` PRIMARY KEY(`id`),
-	CONSTRAINT `name_idx` UNIQUE(`name`)
+	CONSTRAINT `t3-app_post_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `group-project_session` (
+CREATE TABLE `t3-app_session` (
 	`sessionToken` varchar(255) NOT NULL,
 	`userId` varchar(255) NOT NULL,
 	`expires` timestamp NOT NULL,
-	CONSTRAINT `group-project_session_sessionToken` PRIMARY KEY(`sessionToken`)
+	CONSTRAINT `t3-app_session_sessionToken` PRIMARY KEY(`sessionToken`)
 );
 --> statement-breakpoint
-CREATE TABLE `group-project_user` (
+CREATE TABLE `t3-app_user` (
 	`id` varchar(255) NOT NULL,
 	`name` varchar(255),
 	`email` varchar(255) NOT NULL,
 	`emailVerified` timestamp(3) DEFAULT CURRENT_TIMESTAMP(3),
 	`image` varchar(255),
-	CONSTRAINT `group-project_user_id` PRIMARY KEY(`id`)
+	CONSTRAINT `t3-app_user_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `group-project_verificationToken` (
+CREATE TABLE `t3-app_verificationToken` (
 	`identifier` varchar(255) NOT NULL,
 	`token` varchar(255) NOT NULL,
 	`expires` timestamp NOT NULL,
-	CONSTRAINT `group-project_verificationToken_identifier_token` PRIMARY KEY(`identifier`,`token`)
+	CONSTRAINT `t3-app_verificationToken_identifier_token` PRIMARY KEY(`identifier`,`token`)
 );
 --> statement-breakpoint
-CREATE INDEX `userId_idx` ON `group-project_account` (`userId`);--> statement-breakpoint
-CREATE INDEX `userId_idx` ON `group-project_session` (`userId`);
+CREATE INDEX `userId_idx` ON `t3-app_account` (`userId`);--> statement-breakpoint
+CREATE INDEX `createdById_idx` ON `t3-app_post` (`createdById`);--> statement-breakpoint
+CREATE INDEX `name_idx` ON `t3-app_post` (`name`);--> statement-breakpoint
+CREATE INDEX `userId_idx` ON `t3-app_session` (`userId`);
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello.query({ text: "from tRPC" });
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      {hello.greeting}
+    </main>
+  );
+}
