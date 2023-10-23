83bce4571dcc0f86351f545d54640fa3e325ce42
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-10-23 17:33:08 +0200

    feat: added sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { api } from "~/trpc/server";
+// import { api } from "~/trpc/server";
+import { GoogleLoginButton } from "~/components/ui/button";
 
-export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+export default function Home() {
+  // const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      {hello.greeting}
+      <GoogleLoginButton/>
     </main>
   );
 }
