aa623c948f2d2b99ced7613ce146da6aa581040a
Hubert Kowalski <kowalski.hubercik1@gmail.com>
2023-10-23 17:33:00 +0200

    feat: added auth config

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,6 +4,7 @@ import {
   type NextAuthOptions,
   getServerSession,
 } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "~/server/db";
 import { mysqlTable } from "~/server/db/schema";
@@ -46,15 +47,11 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: DrizzleAdapter(db, mysqlTable),
   providers: [
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    // Sign in with Google Oauth provider
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
   ],
 };
 
