563bf33324a5f27051055f6b3aabae458b3001b3
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-10-29 17:19:01 +0100

    turned off some eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,11 @@ const config = {
     "plugin:prettier/recommended",
   ],
   rules: {
+    // These opinionated rules are enabled in stylistic-type-checked above.
+    // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -21,6 +26,12 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
   },
 };
 
