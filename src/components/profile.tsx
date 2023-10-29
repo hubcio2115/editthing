f02510a80a96442bddd4e846b385eba3ae95fd75
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-10-29 17:22:36 +0100

    updated profile component

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -7,11 +7,25 @@ interface ProfileProps {
   session: Session;
 }
 
+function getInitials(name: string) {
+  const firstName = name.split(" ")[0];
+  const lastName = name.split(" ")[1];
+
+  return `${firstName?.at(0)}${lastName?.at(0)}`;
+}
+
 export default function Profile({ session }: ProfileProps) {
   return (
     <Avatar>
-      <AvatarImage className="rounded-full" src={session.user.image ?? ""} alt="profile picture" />
-      <AvatarFallback>CN</AvatarFallback>
+      <AvatarImage
+        // eslint-disable-next-line
+        src={session.user.image!}
+        className="h-10 w-10 rounded-full"
+        alt="profile picture"
+      />
+      <AvatarFallback>
+        {session.user.name ? getInitials(session.user.name) : ""}
+      </AvatarFallback>
     </Avatar>
   );
 }
