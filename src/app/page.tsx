e87ff00a6e550fa394dffb186508f336b972ea16
kalisowski <lisekgda3@gmail.com>
2023-10-29 18:34:09 +0100

    added Pricing Card Components to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { SignOutButton } from "~/components/authButtons";
 import Profile from "~/components/profile";
 import { Button } from "~/components/ui/button";
 import { getServerAuthSession } from "~/server/auth";
+import CardPremium from "~/components/premiumPricingCard";
+import CardStandard from "~/components/standardPricingCard";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -13,10 +15,12 @@ export default async function Home() {
       <Link href="/api/auth/signin">
         <Button>Sign in</Button>
       </Link>
-
       <SignOutButton />
-
       {session ? <Profile session={session} /> : null}
+      <div className="flex gap-10">
+        <CardStandard></CardStandard>
+        <CardPremium></CardPremium>
+      </div>
     </main>
   );
 }
