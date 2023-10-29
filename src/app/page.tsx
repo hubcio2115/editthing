6eb23ba080e71fcd36eb1d613db30f9e2dc084db
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-10-29 17:22:15 +0100

    added navbar to the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import Link from "next/link";
+import Navbar from "~/components/navbar";
 
-import { SignOutButton } from "~/components/authButtons";
-import Profile from "~/components/profile";
-import { Button } from "~/components/ui/button";
-import { getServerAuthSession } from "~/server/auth";
-
-export default async function Home() {
-  const session = await getServerAuthSession();
-
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-10 bg-slate-500 text-white">
-      <Link href="/api/auth/signin">
-        <Button>Sign in</Button>
-      </Link>
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
 
-      <SignOutButton />
-
-      {session ? <Profile session={session} /> : null}
-    </main>
+      <h1>Content</h1>
+    </div>
   );
 }
