3b8f2c698c5c7cae58e4a55957e31d8c0a5b65b0
kalisowski <lisekgda3@gmail.com>
2023-11-02 21:42:46 +0100

    added mockdata for overview page

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -1,7 +1,27 @@
+import { SearchIcon } from "lucide-react";
+
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import VideoCard from "~/components/videoCard";
+import mockdata from "~/lib/mockdata";
+
 export default function Overview() {
   return (
-    <div className="flex min-h-screen flex-col gap-4">
-      Overview of dashboard
+    <div className="mx-auto ">
+      <div className="flex flex-row justify-center gap-4 ">
+        <Button variant="outline" size="icon">
+          <SearchIcon className="mx-2" />
+        </Button>
+        <div className="flex w-full">
+          <Input className="ml-1 mr-5" />
+          <Button variant="outline">Add video</Button>
+        </div>
+      </div>
+      <div className=" mt-5 grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3">
+        {mockdata.map((video, index) => (
+          <VideoCard key={index} video={video} />
+        ))}
+      </div>
     </div>
   );
 }
