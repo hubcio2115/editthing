32427a4f403698b0c7d39b1ebd9504e9c29a0348
kalisowski <lisekgda3@gmail.com>
2023-11-02 21:44:11 +0100

    mock data for dashboard

---
+++ src/lib/mockdata.ts
@@ -0,0 +1,98 @@
+interface MockData {
+  title: string;
+  description: string;
+  thumbnail: string;
+  duration: string;
+  uploaded: string;
+  editor: string;
+}
+
+const mockdata: MockData[] = [
+  {
+    title: "We kidnapped children!",
+    description: "Learn how to create a stunning website from scratch.",
+    thumbnail: "https://i.ytimg.com/vi/vlL-o5DGiY8/maxresdefault.jpg",
+    duration: "12:30",
+    uploaded: "2 days ago",
+    editor: "John Doe",
+  },
+  {
+    title: "We kidnapped children!",
+    description:
+      "Get started with React and build interactive web applications.",
+    thumbnail: "https://i.ytimg.com/vi/TAX6LESYSbA/maxresdefault.jpg",
+    duration: "20:15",
+    uploaded: "1 week ago",
+    editor: "Jane Smith",
+  },
+  {
+    title: "I left my friend to freeze alive",
+    description: "Learn how to create a stunning website from scratch.",
+    thumbnail: "https://i.ytimg.com/vi/vlL-o5DGiY8/maxresdefault.jpg",
+    duration: "12:30",
+    uploaded: "2 days ago",
+    editor: "John Doe",
+  },
+  {
+    title: "We kidnapped children!",
+    description:
+      "Get started with React and build interactive web applications.",
+    thumbnail: "https://i.ytimg.com/vi/TAX6LESYSbA/maxresdefault.jpg",
+    duration: "20:15",
+    uploaded: "1 week ago",
+    editor: "Jane Smith",
+  },
+  {
+    title: "I left my friend to freeze alive",
+    description: "Learn how to create a stunning website from scratch.",
+    thumbnail: "https://i.ytimg.com/vi/vlL-o5DGiY8/maxresdefault.jpg",
+    duration: "12:30",
+    uploaded: "2 days ago",
+    editor: "John Doe",
+  },
+  {
+    title: "We kidnapped children!",
+    description:
+      "Get started with React and build interactive web applications.",
+    thumbnail: "https://i.ytimg.com/vi/TAX6LESYSbA/maxresdefault.jpg",
+    duration: "20:15",
+    uploaded: "1 week ago",
+    editor: "Jane Smith",
+  },
+  {
+    title: "I left my friend to freeze alive",
+    description: "Learn how to create a stunning website from scratch.",
+    thumbnail: "https://i.ytimg.com/vi/vlL-o5DGiY8/maxresdefault.jpg",
+    duration: "12:30",
+    uploaded: "2 days ago",
+    editor: "John Doe",
+  },
+  {
+    title: "We kidnapped children!",
+    description:
+      "Get started with React and build interactive web applications.",
+    thumbnail: "https://i.ytimg.com/vi/TAX6LESYSbA/maxresdefault.jpg",
+    duration: "20:15",
+    uploaded: "1 week ago",
+    editor: "Jane Smith",
+  },
+  {
+    title: "I left my friend to freeze alive",
+    description: "Learn how to create a stunning website from scratch.",
+    thumbnail: "https://i.ytimg.com/vi/vlL-o5DGiY8/maxresdefault.jpg",
+    duration: "12:30",
+    uploaded: "2 days ago",
+    editor: "John Doe",
+  },
+  {
+    title: "We kidnapped children!",
+    description:
+      "Get started with React and build interactive web applications.",
+    thumbnail: "https://i.ytimg.com/vi/TAX6LESYSbA/maxresdefault.jpg",
+    duration: "20:15",
+    uploaded: "1 week ago",
+    editor: "Jane Smith",
+  },
+];
+
+export default mockdata;
