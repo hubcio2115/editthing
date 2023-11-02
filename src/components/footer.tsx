cd085db7e2b109139796cfd241f2672d6c96b687
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-11-02 18:33:11 +0100

    deleted footer

--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="mt-auto flex justify-center bg-slate-200">
-      <div className="max-w-2xl text-fuchsia-900">
-        <h1 className="text-lg font-bold">Footer</h1>
-      </div>
-    </div>
-  );
-}
