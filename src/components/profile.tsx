dde69b3a7215e540efb58d0dbc389d8fb1226920
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-11-03 14:40:03 +0100

    fix profile imports and fallback + style fix

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import type { Session } from "next-auth";
+
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
 interface ProfileProps {
   session: Session;
@@ -11,7 +12,10 @@ function getInitials(name: string) {
   const firstName = name.split(" ")[0];
   const lastName = name.split(" ")[1];
 
-  return `${firstName?.at(0)}${lastName?.at(0)}`;
+  // If username is somehow all blank
+  if (!firstName && !lastName) return "You";
+
+  return `${firstName?.at(0) ?? ""}${lastName?.at(0) ?? ""}`;
 }
 
 export default function Profile({ session }: ProfileProps) {
@@ -20,10 +24,9 @@ export default function Profile({ session }: ProfileProps) {
       <AvatarImage
         // eslint-disable-next-line
         src={session.user.image!}
-        className="h-10 w-10 rounded-full"
         alt="profile picture"
       />
-      <AvatarFallback>
+      <AvatarFallback className="bg-white transition-colors hover:bg-accent hover:text-accent-foreground">
         {session.user.name ? getInitials(session.user.name) : ""}
       </AvatarFallback>
     </Avatar>
