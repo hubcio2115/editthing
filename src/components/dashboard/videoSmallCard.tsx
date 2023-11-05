dcdcdc6001d53b81619278301b6323784730e6a7
kalisowski <lisekgda3@gmail.com>
2023-11-05 22:29:03 +0100

    css changes

--- src/components/dashboard/videoSmallCard.tsx
+++ src/components/dashboard/videoSmallCard.tsx
@@ -21,27 +21,27 @@ interface VideoCardProps {
 
 export default function VideoSmallCard({ video }: VideoCardProps) {
   return (
-    <Card className="flex justify-between hover:cursor-pointer hover:bg-slate-50 2xl:w-[1000px]">
-      <div className="m-0 flex">
+    <Card className="flex flex-col justify-between hover:cursor-pointer hover:bg-slate-50 sm:flex-row 2xl:w-[1000px]">
+      <div className="m-0 flex flex-col sm:flex-row">
         <CardContent>
-          <div className="relative mt-5">
+          <div className="relative mt-5 hidden h-16 w-28 sm:flex">
             <img
               src={video.thumbnail}
               alt="Video Thumbnail"
-              className="h-18 w-32 object-cover"
+              className="object-cover"
             />
-            <div className="absolute bottom-0 right-0 bg-black  text-white ">
+            <div className="absolute bottom-0 left-0 bg-black text-white ">
               {video.duration}
             </div>
           </div>
         </CardContent>
-        <CardHeader>
-          <CardTitle>{video.title}</CardTitle>
+        <CardHeader className="py-0 sm:py-5">
+          <CardTitle className="text-base sm:text-2xl">{video.title}</CardTitle>
           <CardDescription>{video.description}</CardDescription>
         </CardHeader>
       </div>
-      <CardFooter className="mt-5 flex flex-col items-end justify-between">
-        <Label>Editor: {video.editor}</Label>
+      <CardFooter className="flex flex-col items-end justify-between sm:mt-5">
+        <Label className="whitespace-nowrap">Editor: {video.editor}</Label>
         <Label>{video.uploaded}</Label>
       </CardFooter>
     </Card>
