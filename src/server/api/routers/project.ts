dd7815f41937ccd6db173bc852109a64d8015973
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-28 21:31:06 +0100

    changed projectSchema to infer itself from mysql table definition

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,28 +1,12 @@
 import { eq } from "drizzle-orm";
+import { createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
 import { projects } from "~/server/db/schema";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
-const projectSchema = z.object({
-  projectName: z.string().max(256),
-  projectDescription: z.string().max(512).optional(),
-  title: z.string().max(256).optional(),
-  description: z.string().max(512).optional(),
-  categoryId: z.string().max(128).optional(),
-  defaultLanguage: z.string().max(128).optional(),
-  embeddable: z.boolean().optional(),
-  license: z.enum(["youtube", "creativeCommon"]).optional(),
-  privacyStatus: z.enum(["public", "unlisted", "private"]).optional(),
-  publicStatsViewable: z.boolean().optional(),
-  publishAt: z
-    .string()
-    .refine((value) => !isNaN(Date.parse(value)))
-    .optional(),
-  selfDeclaredMadeForKids: z.boolean().optional(),
-  organizationId: z.number(),
-});
+const projectSchema = createSelectSchema(projects);
 
 export const projectRouter = createTRPCRouter({
   getOrgProjects: protectedProcedure
