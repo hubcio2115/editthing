71f44f354eaca3f19dfeae8268e744a81c8e4131
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-28 20:34:16 +0100

    added a form for creating new organizaitons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,56 +1,176 @@
-import { LogOut } from "lucide-react";
-import Link from "next/link";
+"use client";
 
-import { getServerAuthSession } from "~/server/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LogOut } from "lucide-react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import {
+  type SubmitErrorHandler,
+  type SubmitHandler,
+  useForm,
+} from "react-hook-form";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import {
+  type InsertOrganization,
+  insertOrganizationSchema,
+} from "~/lib/validators/organization";
+import { api } from "~/trpc/react";
 
 import { SignOutButton } from "./authButtons";
 import Profile from "./profile";
 import { Button } from "./ui/button";
 import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
-export default async function Navbar() {
-  const session = await getServerAuthSession();
+type OrganizationForm = Omit<InsertOrganization, "owner">;
+
+export default function Navbar() {
+  const router = useRouter();
+  const session = useSession();
+  const { data: organizations } =
+    api.organization.getOwnOrganizations.useQuery();
+
+  const { mutate: createOrganization } =
+    api.organization.createOrganization.useMutation({
+      onSuccess: (data) => {
+        if (!!data) router.push(`/dashboard/${data.id}`);
+      },
+    });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { register, handleSubmit } = useForm<OrganizationForm>({
+    resolver: zodResolver(insertOrganizationSchema.omit({ owner: true })),
+  });
+
+  const onSubmit: SubmitHandler<OrganizationForm> = (data) => {
+    // createOrganization({ name: data.name });
+    console.log(data);
+  };
+
+  const onError: SubmitErrorHandler<OrganizationForm> = (error) => {
+    console.error(error);
+  };
 
   return (
-    <nav className="flex items-center justify-between border-b border-slate-200 bg-slate-100 p-2">
-      <h1 className="text-3xl font-bold text-gray-900">
-        <span className="text-fuchsia-900">Edit</span>
-        thing
-      </h1>
+    <>
+      <div className="flex w-full items-center justify-between">
+        <div className="flex gap-4">
+          <h1 className="text-3xl font-bold text-gray-900">
+            <span className="text-fuchsia-900">Edit</span>
+            thing
+          </h1>
 
-      <div>
-        {session ? (
-          <div className="flex gap-2">
-            <DropdownMenu>
-              <DropdownMenuTrigger className="focus:outline-none">
-                <Profile session={session} />
-              </DropdownMenuTrigger>
+          {session.status === "authenticated" && (
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select a fruit" />
+              </SelectTrigger>
 
-              <DropdownMenuContent>
-                <DropdownMenuItem>
-                  <LogOut className="mr-2 h-4 w-4" color="red" />
-                  <SignOutButton>Log out</SignOutButton>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+              <SelectContent>
+                <SelectGroup>
+                  {organizations?.map((org) => (
+                    <SelectItem value={org.name} key={org.id}>
+                      {org.name}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          )}
+        </div>
 
-            <Link href="/dashboard/overview">
-              <Button variant="outline" className="rounded-full">
-                Go to Dashboard
-              </Button>
+        <div>
+          {session.status === "authenticated" ? (
+            <div className="flex gap-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger className="focus:outline-none">
+                  <Profile session={session.data} />
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent className="w-[200px]">
+                  <DropdownMenuItem
+                    onClick={() => {
+                      setIsModalOpen(true);
+                    }}
+                  >
+                    Create Organization
+                  </DropdownMenuItem>
+
+                  <DropdownMenuSeparator />
+
+                  <DropdownMenuItem>
+                    <LogOut className="mr-2 h-4 w-4" color="red" />
+                    <SignOutButton>Log out</SignOutButton>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              <Link href="/dashboard/overview">
+                <Button variant="outline" className="rounded-full">
+                  Go to Dashboard
+                </Button>
+              </Link>
+            </div>
+          ) : (
+            <Link href="/api/auth/signin">
+              <Button variant="ghost">Sign in</Button>
             </Link>
-          </div>
-        ) : (
-          <Link href="/api/auth/signin">
-            <Button variant="ghost">Sign in</Button>
-          </Link>
-        )}
+          )}
+        </div>
       </div>
-    </nav>
+
+      <Dialog
+        open={isModalOpen}
+        onOpenChange={(open) => {
+          setIsModalOpen(open);
+        }}
+      >
+        <DialogContent className="sm:max-w-[425px]">
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit, onError)}
+          >
+            <DialogHeader>
+              <DialogTitle>New Organization</DialogTitle>
+            </DialogHeader>
+
+            <div className="mt-4 flex flex-col gap-2">
+              <Label htmlFor="name">Name</Label>
+
+              <Input {...register("name")} id="name" />
+            </div>
+
+            <DialogFooter>
+              <Button type="submit">Create</Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
