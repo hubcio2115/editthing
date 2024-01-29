f91b2453f4f83dc2d6cd127dd34faf8fa22428c9
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2024-01-29 05:27:24 +0100

    update mock data to simulate db relation

--- src/lib/mock/organizations.ts
+++ src/lib/mock/organizations.ts
@@ -8,18 +8,13 @@ interface OrgMockData {
 const organizationsMock: OrgMockData[] = [
   {
     id: "7b74f4d5-ef62-4c55-91fc-73eeb6a2f700",
-    name: "Organization 1",
+    name: "MrBeast",
     ownerId: "1fb6b3d1-0bc7-4a7a-b7f3-79ab7e9d5cb9",
   },
   {
     id: "a1742b12-40a0-48bb-8c42-02d1680c5d0a",
-    name: "Organization 2",
+    name: "spryciarze.pl",
     ownerId: "e6d6bc24-6f4d-4f40-b7f1-ded2f7a72ac8",
-  },
-  {
-    id: "b625e729-71f8-4b0f-a792-8841bfed827e",
-    name: "Organization 3",
-    ownerId: "c67c4dd3-0e5a-4674-83ff-cf8a1e5c3db2",
   },
 ];
 
