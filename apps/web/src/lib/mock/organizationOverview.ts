d1482134a4f8ddf865151d43f142e0da239d2eec
kalisowski <lisekgda3@gmail.com>
2024-03-25 15:44:12 +0100

    chore: removed unused types

--- apps/web/src/lib/mock/organizationOverview.ts
+++ apps/web/src/lib/mock/organizationOverview.ts
@@ -1,7 +1,4 @@
-interface MockData {
-  orgId: string;
-  projects: ProjectMockData[];
-}
+import type { mock } from "node:test";
 
 interface ProjectMockData {
   title: string;
@@ -100,24 +97,4 @@ const projectMockData = [...] as const;
   },
 ];
 
-const orgs: MockData[] = [
-  {
-    orgId: "7b74f4d5-ef62-4c55-91fc-73eeb6a2f700",
-    projects: mockdata1,
-  },
-  {
-    orgId: "a1742b12-40a0-48bb-8c42-02d1680c5d0a",
-    projects: [
-      {
-        title: "How to get a girlfriend guide",
-        description: "unregistered hypercam edition",
-        thumbnail: "https://i.ytimg.com/vi/pV0dCyO3Jps/maxresdefault.jpg",
-        duration: "21:07",
-        uploaded: "11 days ago",
-        editor: "TheLegend2137",
-      },
-    ],
-  },
-];
-
-export default orgs;
+export default mockdata1;
