c2d83eb44adc99e84e759042836794231dd95bf6
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-03-30 14:28:50 +0100

    fix build error

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 
 import { api } from "~/trpc/server";
 
+export const dynamic = "force-dynamic";
+
 export default async function Dashboard() {
   const organizations = await api.organization.getOwnOrganizations.query();
   const orgNames = organizations.map((org) => org.name!);
