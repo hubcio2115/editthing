fffe5e8886b44e040bc0d1880a701dea2f6836d8
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-03-30 14:13:44 +0100

    updated Dashboard to redirect to first organizaion dashboard

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,3 +1,10 @@
-export default function Dashboard() {
-  return <div></div>;
+import { redirect } from "next/navigation";
+
+import { api } from "~/trpc/server";
+
+export default async function Dashboard() {
+  const organizations = await api.organization.getOwnOrganizations.query();
+  const orgNames = organizations.map((org) => org.name!);
+
+  redirect(`/dashboard/${orgNames[0]}/overview`);
 }
