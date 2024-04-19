5a043d09c8cad5d10976e399d521ea00dba474d8
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-04-19 19:50:23 +0200

    changed trpc queries to react-queries

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import { redirect } from "next/navigation";
 
-import { api } from "~/trpc/server";
+import { getOwnOrganizations } from "~/server/actions/organization";
 
 export const dynamic = "force-dynamic";
 
 export default async function Dashboard() {
-  const organizations = await api.organization.getOwnOrganizations.query();
+  const organizations = await getOwnOrganizations();
   const orgNames = organizations.map((org) => org.name!);
 
   redirect(`/dashboard/${orgNames[0]}/overview`);
