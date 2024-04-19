5c3cf6e2a6726f9a5c86eeb609c9b24639746bcd
kalisowski <lisekgda3@gmail.com>
2024-04-19 02:49:46 +0200

    changed to server component

--- apps/web/src/app/dashboard/[name]/page.tsx
+++ apps/web/src/app/dashboard/[name]/page.tsx
@@ -1,13 +1,17 @@
-"use client";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { api } from "~/trpc/server";
 
-export default function OrganizationView() {
-  const router = useRouter();
-  const pathname = usePathname();
-  useEffect(() => {
-    router.push(`${pathname}/overview`);
-  });
-  return <></>;
+export default async function Organization() {
+  const heads = headers();
+  const orgFromPath = heads.get("next-url")?.split("/").at(2)!;
+  const organizations = await api.organization.getOwnOrganizations.query();
+  const orgNames = organizations.map((org) => org.name!);
+
+  if (orgNames.includes(orgFromPath)) {
+    redirect(`/dashboard/${orgFromPath}/overview`);
+  } else {
+    redirect("/dashboard");
+  }
 }
