160ab8fec398a97b7df84aa979b24411ede50ac2
kalisowski <lisekgda3@gmail.com>
2024-04-19 02:57:06 +0200

    WIP: new schemas for orgs

--- apps/web/src/server/api/routers/organization.ts
+++ apps/web/src/server/api/routers/organization.ts
@@ -1,6 +1,11 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
+import { createSelectSchema } from "drizzle-zod";
+import { string, z } from "zod";
 
-import { insertOrganizationSchema } from "~/lib/validators/organization";
+import {
+  insertOrganizationSchema,
+  updateOrganizationNameSchema,
+} from "~/lib/validators/organization";
 import { createOrganization } from "~/server/db/organizations";
 import { organizations, usersToOrganizations } from "~/server/db/schema";
 
@@ -13,6 +18,16 @@ export const organizationRouter = createTRPCRouter({
       .from(organizations)
       .where(eq(organizations.owner, ctx.session.user.id));
   }),
+
+  getOwnOrganizationByName: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      return ctx.db.query.organizations.findFirst({
+        where({ owner, name }, { and, eq }) {
+          return and(eq(owner, ctx.session.user.id), eq(name, input));
+        },
+      });
+    }),
 
   getOrganizations: protectedProcedure.query(async ({ ctx }) => {
     return ctx.db
@@ -29,15 +44,36 @@ export const organizationRouter = createTRPCRouter({
       .where(eq(usersToOrganizations.memberId, ctx.session.user.id));
   }),
 
+  // TODO: Implement this
+  getMembersOfOrganization: protectedProcedure.query(async ({ ctx }) => {}),
+
   createOrganization: protectedProcedure
     .input(insertOrganizationSchema)
     .mutation(async ({ ctx, input }) => {
-      const newOrganization = (await createOrganization(
-        ctx.db,
-        input.name,
-        ctx.session.user.id,
-      ))[0];
+      const newOrganization = (
+        await createOrganization(ctx.db, input.name, ctx.session.user.id)
+      )[0];
 
       return newOrganization;
+    }),
+
+  updateOrganizationName: protectedProcedure
+    .input(updateOrganizationNameSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db
+        .update(organizations)
+        .set({ name: input.name })
+        .where(
+          and(
+            eq(organizations.name, input.oldName),
+            eq(organizations.owner, ctx.session.user.id),
+          ),
+        );
+    }),
+
+  deleteOrganization: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.delete(organizations).where(eq(organizations.name, input));
     }),
 });
