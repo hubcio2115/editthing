30f0374ef96896a35e937d474f28000e1a6711aa
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-04-19 19:45:51 +0200

    updated pacakges and added eslint-plugin-drizzle

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -6,11 +6,12 @@ const config = {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "drizzle"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/strict-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:drizzle/all",
     "plugin:prettier/recommended",
   ],
   rules: {
