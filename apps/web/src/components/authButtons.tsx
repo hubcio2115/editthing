d287ce5aafbd88afccdcac165cf5c3c60153ea8d
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-04-24 13:42:49 +0200

    deleted authButtons

--- apps/web/src/components/authButtons.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { signOut } from "next-auth/react";
-import { type PropsWithChildren } from "react";
-
-// NOTE: This might not be necessary anymore, it's marked for deletion
-export function SignOutButton({ children }: PropsWithChildren) {
-  return <span onClick={() => signOut()}>{children}</span>;
-}
