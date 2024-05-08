971c5a696cf64268ef5f21a9c90cc6677da851b4
kalisowski <lisekgda3@gmail.com>
2024-05-08 19:19:34 +0200

    added type for role enum and adjusted org schema

--- apps/web/src/lib/validators/organization.ts
+++ apps/web/src/lib/validators/organization.ts
@@ -1,20 +1,21 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
+import { roleEnum as role } from "~/server/db/schema";
 import { organizations } from "~/server/db/schema";
 
 const orgNameRegex = /^[a-zA-Z0-9-]+$/;
 
-export const insertOrganizationSchema = createInsertSchema(organizations)
-  .omit({ owner: true })
-  .extend({
-    name: z
-      .string()
-      .min(3, "Name must contain at least 3 characters")
-      .refine((name) => orgNameRegex.test(name), {
-        message: "Name can only contain alphanumeric characters and '-' sign",
-      }),
-  });
+export const insertOrganizationSchema = createInsertSchema(
+  organizations,
+).extend({
+  name: z
+    .string()
+    .min(3, "Name must contain at least 3 characters")
+    .refine((name) => orgNameRegex.test(name), {
+      message: "Name can only contain alphanumeric characters and '-' sign",
+    }),
+});
 
 export const updateOrganizationNameSchema = insertOrganizationSchema
   .pick({
@@ -33,3 +34,17 @@ export type Organization = z.infer<typeof organizationSchema>;
 export type UpdateOrganizationName = z.infer<
   typeof updateOrganizationNameSchema
 >;
+
+export const roleEnum = z.enum(role.enumValues);
+
+export type organizationWithMembers =
+  | {
+      id: number;
+      name: string;
+      defaultOrg: boolean;
+      memberId: string;
+      memberName: string;
+      memberEmail: string;
+      memberRole: z.infer<typeof roleEnum>;
+    }
+  | undefined;
