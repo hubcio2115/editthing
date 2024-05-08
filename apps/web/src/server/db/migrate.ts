3ef9f316aababe628732687bfe2fac206fdd15a1
kalisowski <lisekgda3@gmail.com>
2024-05-08 19:20:14 +0200

    added view auto migration

--- apps/web/src/server/db/migrate.ts
+++ apps/web/src/server/db/migrate.ts
@@ -1,5 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { config } from "dotenv";
+import { sql as sqlD } from "drizzle-orm";
+import { PgView, getViewConfig, pgView } from "drizzle-orm/pg-core";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { migrate } from "drizzle-orm/vercel-postgres/migrator";
 
@@ -12,6 +14,13 @@ export const db = drizzle(sql, { schema });
 const main = async () => {
   try {
     await migrate(db, { migrationsFolder: ".drizzle" });
+    for (const view of Object.values(schema)) {
+      if (view instanceof PgView) {
+        db.execute(sqlD`drop view if exists ${view}`);
+        db.execute(sqlD`create view ${view} as ${getViewConfig(view).query}`);
+      }
+    }
+    console.log(`Views created`);
     console.log("Migration completed");
   } catch (error) {
     console.error("Error during migration:", error);
