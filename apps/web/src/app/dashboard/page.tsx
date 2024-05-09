21795dcfab8ca7d73edd9d12e2e4105b22b1201f
kalisowski <lisekgda3@gmail.com>
2024-05-09 05:51:37 +0200

    added a view for a user that somehow ended up without organizations

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-
+import { Button } from "~/components/ui/button";
 import {
   getOwnOrganizations,
 } from "~/server/actions/organization";
@@ -9,8 +9,19 @@ export default async function Dashboard() {
 
   if (err !== null) {
     throw err;
-  } else {
+  } else if (organizations.length !== 0) {
     const orgNames = organizations.map((org) => org.name);
     redirect(`/dashboard/${orgNames[0]}/overview`);
   }
+
+  return (
+    <div className="flex flex-col items-center  justify-center min-h-[calc(100vh-73px)] w-full">
+      <div className="flex flex-col items-center w-[280px]  sm:w-[500px] gap-4 p-10 bg-slate-50 border rounded-lg">
+        <h1 className="text-2xl font-bold text-gray-900"><span className="text-fuchsia-900">Edit</span>thing</h1>
+        <h2 className="text-center font-semibold">Create an organization to get started</h2>
+        <p className="text-center">An organization is where you create projects and add team members. You can add multiple organiations in your account for different collaboration projects. </p>
+
+      </div>
+    </div >
+  )
 }
