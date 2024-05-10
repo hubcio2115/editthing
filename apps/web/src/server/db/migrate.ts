812008d237bd6bedbed2a7065f39e1e944b5756f
kalisowski <lisekgda3@gmail.com>
2024-05-10 17:19:02 +0200

    deleted non-working part

--- apps/web/src/server/db/migrate.ts
+++ apps/web/src/server/db/migrate.ts
@@ -1,7 +1,5 @@
 import { sql } from "@vercel/postgres";
 import { config } from "dotenv";
-import { sql as sqlD } from "drizzle-orm";
-import { PgView, getViewConfig, pgView } from "drizzle-orm/pg-core";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { migrate } from "drizzle-orm/vercel-postgres/migrator";
 
@@ -14,13 +12,6 @@ export const db = drizzle(sql, { schema });
 const main = async () => {
   try {
     await migrate(db, { migrationsFolder: ".drizzle" });
-    for (const view of Object.values(schema)) {
-      if (view instanceof PgView) {
-        db.execute(sqlD`drop view if exists ${view}`);
-        db.execute(sqlD`create view ${view} as ${getViewConfig(view).query}`);
-      }
-    }
-    console.log(`Views created`);
     console.log("Migration completed");
   } catch (error) {
     console.error("Error during migration:", error);
