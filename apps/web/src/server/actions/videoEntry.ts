530b929c9a5bb7784fcafff904cea5e22365e1d4
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-05-13 21:57:20 +0200

    added videoEntry actions

---
+++ apps/web/src/server/actions/videoEntry.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import type { Result } from "~/lib/utils";
+
+import { db } from "../db";
+import { videoEntries } from "../db/schema";
+import type { InsertVideoEntry, VideoEntry } from "../db/videoEntry";
+
+export async function createVideoEntry(
+  entry: InsertVideoEntry,
+): Promise<Result<VideoEntry>> {
+  try {
+    const newEntry = (
+      await db.insert(videoEntries).values(entry).returning()
+    )[0]!;
+
+    return [newEntry, null];
+  } catch (err) {
+    return [null, (err as Error).message];
+  }
+}
