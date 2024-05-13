f42507fc5cc69616ba39e1a6386f547cdaa8c01f
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-05-13 22:05:26 +0200

    added create-project page

--- apps/web/src/app/dashboard/[name]/create-project/page.tsx
+++ apps/web/src/app/dashboard/[name]/create-project/page.tsx
@@ -1,68 +1,30 @@
-"use client";
-
-import MuxUploader, {
-  MuxUploaderDrop,
-  MuxUploaderFileSelect,
-  MuxUploaderProgress,
-} from "@mux/mux-uploader-react";
-import { VideoIcon } from "lucide-react";
-import { useSession } from "next-auth/react";
-
-import { Button } from "~/components/ui/button";
+import VideUploadForm from "~/components/create-project/create-project-form";
 import { createEndpointForMuxUpload } from "~/server/actions/mux";
+import { getOwnOrganizationByName } from "~/server/actions/organization";
+import { getServerAuthSession } from "~/server/auth";
 
-export default function CreateProjectPage() {
-  const session = useSession();
+type CreateProjectPageProps = {
+  params: {
+    name: string;
+  };
+};
 
-  async function endpointHandler() {
-    const [upload, err] = await createEndpointForMuxUpload(
-      session.data!.user.id,
-    );
+export default async function CreateProjectPage({
+  params,
+}: CreateProjectPageProps) {
+  const session = await getServerAuthSession();
 
-    if (err !== null) {
-      console.error(err.message);
-      return "";
-    }
-
-    return upload.url;
+  const [upload, uploadErr] = await createEndpointForMuxUpload(
+    session!.user.id,
+  );
+  if (uploadErr !== null) {
+    throw new Error(uploadErr);
   }
 
-  return (
-    <div className="container max-w-[800px] flex-1">
-      <MuxUploader
-        id="uploader"
-        className="hidden"
-        endpoint={endpointHandler}
-      />
+  const [org, err] = await getOwnOrganizationByName(params.name);
+  if (err !== null) {
+    throw new Error(err);
+  }
 
-      <MuxUploaderDrop
-        id="uploader"
-        className="mt-2 flex justify-center gap-2 rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
-        overlay
-        overlayText="Let it go"
-      >
-        <span slot="heading" className="text-xl text-slate-600">
-          <VideoIcon
-            className="mx-auto h-12 w-12 text-gray-300"
-            aria-hidden="true"
-          />
-          Drag and drop video file to upload
-        </span>
-
-        <span slot="separator" className="text-sm italic text-slate-400">
-          - or -
-        </span>
-
-        <MuxUploaderFileSelect muxUploader="uploader">
-          <Button>Select file</Button>
-        </MuxUploaderFileSelect>
-      </MuxUploaderDrop>
-
-      <MuxUploaderProgress
-        type="bar"
-        muxUploader="uploader"
-        className="text-orange-600"
-      />
-    </div>
-  );
+  return <VideUploadForm upload={upload} org={org} />;
 }
