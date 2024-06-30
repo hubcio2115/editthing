cdbcee88159e60f161c44ab6e7565542948dc4bb
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-06-30 16:55:57 +0200

    temp page for fetching youtube data

--- apps/web/src/app/dashboard/[name]/project/[id]/page.tsx
+++ apps/web/src/app/dashboard/[name]/project/[id]/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "~/components/ui/button";
+import { getProjectById } from "~/server/actions/project";
+import {
+  getUserChannels,
+  uploadAnExampleVideo,
+} from "~/server/actions/yt/channels";
+
 type ProjectPageProps = {
   params: {
     name: string;
@@ -5,14 +12,36 @@ type ProjectPageProps = {
   };
 };
 
-export default function ProjectPage({
+export default async function ProjectPage({
   params: { name, id },
 }: ProjectPageProps) {
+  const [project, err] = await getProjectById(+id);
+  const [channels] = await getUserChannels();
+
+  if (err !== null) {
+    throw new Error(`Error during fetching the project: ${err}`);
+  }
+
+  const channel = channels?.at(0);
+
   return (
     <div>
+      <p>{project.id}</p>
+      <img
+        className="rounded-full overflow-hidden"
+        src={channel?.snippet.thumbnails.default.url}
+        height={channel?.snippet.thumbnails.default.height}
+        width={channel?.snippet.thumbnails.default.width}
+      />
+
       <p>
-        {id}: {name}
+        Name: {channel?.snippet.title} {channel?.snippet.customUrl}
       </p>
+      <p>Description: {channel?.snippet.description}</p>
+
+      <form action={uploadAnExampleVideo}>
+        <Button type="submit">Upload</Button>
+      </form>
     </div>
   );
 }
