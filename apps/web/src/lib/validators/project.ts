e8f105628348b68306bfff9a7dc15f28a5b315b6
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-07-31 21:56:48 +0200

    added contraint for tags

--- apps/web/src/lib/validators/project.ts
+++ apps/web/src/lib/validators/project.ts
@@ -3,7 +3,13 @@ import { z } from "zod";
 
 import { projects } from "~/server/db/schema";
 
-export const insertProjectSchema = createInsertSchema(projects);
+export const insertProjectSchema = createInsertSchema(projects).extend({
+  tags: z
+    .string()
+    .max(500)
+    .regex(/^([a-zA-z]*)|([a-zA-Z]+[a-zA-Z,]*)$/, "Tags can consist only from latin letters.")
+    .nullable(),
+});
 
 export type InsertProject = z.infer<typeof insertProjectSchema>;
 
