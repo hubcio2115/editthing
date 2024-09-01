1c85bbdce35a6f4b05e4519e8eddb32fe0f62f18
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-09-01 17:40:45 +0200

    final create-project page

--- apps/web/src/app/dashboard/[name]/create-project/page.tsx
+++ apps/web/src/app/dashboard/[name]/create-project/page.tsx
@@ -1,13 +1,28 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import ProjectForm from "~/components/create-project/project-form";
+import { useCreateProjectMutation } from "~/lib/mutations/useCreateProjectMutation";
 
-export default async function CreateProjectPage() {
-  function onSuccess() {}
+interface CreateProjectPageProps {
+  params: { name: string };
+}
+
+export default function CreateProjectPage({ params }: CreateProjectPageProps) {
+  const router = useRouter();
+
+  const mutation = useCreateProjectMutation(params.name, {
+    onSuccess: (project) => {
+      router.push(`/dashboard/${params.name}/project/${project.id}`);
+    },
+    onError: (err) => {
+      console.error(err);
+    },
+  });
 
   return (
     <div className="container flex max-w-[800px] flex-1 flex-col items-center pb-11">
-      <ProjectForm onSuccess={onSuccess} />
+      <ProjectForm mutation={mutation} />
     </div>
   );
 }
