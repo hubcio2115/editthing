7aa83cd93a23adaa9cbecd1aaa9c666ae9504de8
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-09-01 17:41:09 +0200

    added a route for redirecting from /dashboard to defaultOrg

---
+++ apps/web/src/app/dashboard/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getDefaultUserOrganization } from "~/server/api/utils/organizations";
+import { auth } from "~/server/auth";
+
+export async function GET(req: NextRequest) {
+  const session = await auth();
+
+  const org = (await getDefaultUserOrganization(session!.user.id)).at(0);
+
+  const url = req.nextUrl.clone();
+
+  url.pathname = org ? `/dashboard/${org.organizations?.name}/overview` : "/404";
+
+  return NextResponse.redirect(url);
+}
