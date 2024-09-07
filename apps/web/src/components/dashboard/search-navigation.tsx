1a02b88f6df796d13a2261e4e59dad265808ea97
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-09-07 15:07:17 +0200

    added search functionality

--- apps/web/src/components/dashboard/search-navigation.tsx
+++ apps/web/src/components/dashboard/search-navigation.tsx
@@ -1,53 +1,49 @@
 "use client";
 
-import { SearchIcon, StretchHorizontal } from "lucide-react";
+import { SearchIcon } from "lucide-react";
 import Link from "next/link";
 import { Input } from "~/components/ui/input";
-import { Toggle } from "~/components/ui/toggle";
 import { Button } from "~/components/ui/button";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState, type ChangeEvent } from "react";
+import { useDebounce } from "@uidotdev/usehooks";
 
 interface ProjectGridProps {
   orgName: string;
 }
 
 export default function SearchNavigation({ orgName }: ProjectGridProps) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
   const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  const listDisplayType = searchParams.get("listType");
+  const [query, setQuery] = useState(searchParams.get("q") ?? "");
+  const debouncedQuery = useDebounce(query, 300);
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    params.set("q", debouncedQuery);
+
+    router.push(pathname + "?" + params.toString());
+  }, [debouncedQuery]);
+
+  function onChange(event: ChangeEvent<HTMLInputElement>) {
+    setQuery(event.target.value);
+  }
 
   return (
-    <div className="flex">
+    <div className="flex max-w-[700px] w-full">
       <div className="flex flex-1">
         <div className="rounded-lg rounded-r-none border border-r-0">
           <SearchIcon className="m-2 h-5 w-6" />
         </div>
-        <Input className="mr-2 rounded-l-none border-l-0 focus:outline-none" />
+
+        <Input
+          onChange={onChange}
+          className="mr-2 rounded-l-none border-l-0 focus:outline-none"
+          placeholder="Filter by title..."
+        />
       </div>
-
-      <Toggle
-        variant="outline"
-        onClick={() => {
-          const params = new URLSearchParams(searchParams);
-
-          switch (listDisplayType) {
-            case "list":
-              params.set("listType", "grid");
-              break;
-            case "grid":
-            default:
-              params.set("listType", "list");
-              break;
-          }
-
-          router.replace(`${pathname}?${params.toString()}`);
-        }}
-        className="mr-2"
-      >
-        <StretchHorizontal />
-      </Toggle>
 
       <Link href={`/dashboard/${orgName}/create-project`}>
         <Button variant="outline">Add video</Button>
