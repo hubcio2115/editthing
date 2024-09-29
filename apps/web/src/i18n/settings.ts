511ce3300419d68efd6bdb9cb986fb54bc546be4
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-09-29 11:32:36 +0200

    added SUPPORTED_LANGUAGES constant

--- apps/web/src/i18n/settings.ts
+++ apps/web/src/i18n/settings.ts
@@ -1,11 +1,18 @@
 export const FALLBACK_LANG = "en";
-export const LANGUAGES = ["en", "pl"] as const;
+export const SUPPORTED_LANGUAGES = [
+  { value: "en", label: "🇬🇧 English" },
+  { value: "pl", label: "🇵🇱 Polski" },
+] as const;
+export const LANGUAGES = SUPPORTED_LANGUAGES.map((lang) => lang.value);
 export const DEFAULT_NS = "translation";
 export const COOKIE_NAME = "i18next";
 
 export type SupportedLanguages = (typeof LANGUAGES)[number];
 
-export function getOptions (lang: SupportedLanguages = FALLBACK_LANG, ns: string | string[] = DEFAULT_NS) {
+export function getOptions(
+  lang: SupportedLanguages = FALLBACK_LANG,
+  ns: string | string[] = DEFAULT_NS,
+) {
   return {
     debug: true,
     supportedLngs: LANGUAGES,
@@ -15,5 +22,5 @@ export function getOptions (lang: SupportedLanguages = FALLBACK_LANG, ns: string
     fallbackNS: DEFAULT_NS,
     defaultNS: DEFAULT_NS,
     ns,
-  }
+  };
 }
